a69b389ddf49e077bed3954080c18c02e7d2df3b
rkp7618 <rk7618@srmist.edu.in>
2024-07-01 10:10:26 +0530

    initial commit

---
+++ enums/supportedLanguages.js
@@ -0,0 +1,15 @@
+module.exports = {
+    C: 'c',
+    CPP: 'cpp',
+    PYTHON: 'python',
+    JAVA: 'java',
+    NODEJS: 'nodejs',
+    RUBY: 'ruby',
+    GO: 'go',
+    PHP: 'php',
+    RUST: 'rust',
+    PROMPTV1: 'promptv1',
+    PROMPTV2: 'promptv2',
+    MULTIFILE: 'multifile',
+    SQLITE3: 'sqlite3',
+}
